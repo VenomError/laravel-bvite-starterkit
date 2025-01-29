5a8fb264ed75660c42dd084b4e865a1dc93ced09
VenomError <venoms00001@gmail.com>
2025-01-29 21:46:44 +0800

    Add/Feat : Rightbar Components

--- resources/views/components/app/header/menu.blade.php
+++ resources/views/components/app/header/menu.blade.php
@@ -200,36 +200,7 @@
             <img class="avatar rounded-circle border border-3" src="assets/images/profile_av.png" alt="avatar">
             <span class="ms-2 fs-6 d-none d-sm-inline-flex">Michelle</span>
         </a>
-        <div class="dropdown-menu dropdown-menu-end shadow p-2 p-xl-3 rounded-4">
-            <div class="bg-body p-3 rounded-3">
-                <h4 class="mb-1 title-font text-gradient">Michelle Glover</h4>
-                <p class="small text-muted">michelle.glover@gmail.com</p>
-                <input type="text" class="form-control form-control-sm" placeholder="Update my status">
-            </div>
-            <ul class="list-unstyled mt-3">
-                <li>
-                    <a class="dropdown-item rounded-pill" aria-label="my wallet" href="my-wallet.html">
-                        <span class="align-middle">Balance: <span class="fw-bold text-success"><span
-                                    data-purecounter-start="0" data-purecounter-separator=","
-                                    data-purecounter-currency="$" data-purecounter-end="14000"
-                                    class="purecounter">0</span></span></span>
-                    </a>
-                </li>
-                <li><a class="dropdown-item rounded-pill" aria-label="my profile" href="my-profile.html">My
-                        Profile</a></li>
-                <li><a class="dropdown-item rounded-pill" aria-label="my task" href="my-task.html">My
-                        Taskboard</a></li>
-                <li><a class="dropdown-item rounded-pill" aria-label="account settings"
-                        href="account-settings.html">Settings</a></li>
-                <li class="dropdown-divider"></li>
-                <li><a class="dropdown-item rounded-pill" aria-label="Add another account" href="#">Add another
-                        account</a></li>
-            </ul>
-            <a class="btn py-2 btn-primary w-100 mt-3 rounded-pill" href="signin.html" role="button">Logout</a>
-            <div class="mt-3 text-center small">
-                <a class="text-muted me-1" href="#!">Privacy policy</a>•<a class="text-muted mx-1"
-                    href="#!">Terms</a>•<a class="text-muted ms-1" href="#!">Cookies</a>
-            </div>
-        </div>
+        
+        <x-app.profile.action />
     </li>
 </ul>
--- resources/views/components/app/logo.blade.php
+++ resources/views/components/app/logo.blade.php
@@ -1,155 +1,50 @@
- <div class="px-md-4 px-2 pt-2 pb-2 brand" data-bs-theme="none">
-     <div>
-         <div class="d-flex align-items-center pt-1">
-             <button class="btn d-inline-flex d-xl-none border-0 p-0 pe-2" type="button" data-bs-toggle="offcanvas"
-                 data-bs-target="#offcanvas_Navbar">
-                 <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="26" height="26"
-                     viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round"
-                     stroke-linejoin="round">
-                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                     <path d="M4 6l16 0"></path>
-                     <path d="M4 12l16 0"></path>
-                     <path d="M4 18l16 0"></path>
-                 </svg>
-             </button>
-             <!--[ Start:: Brand Logo icon ]-->
-             <a href="index.html" class="brand-icon text-decoration-none d-flex align-items-center"
-                 title="BVITE Admin Template">
-                 <svg height="26" viewBox="0 0 40 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                     <path fill="var(--primary-color)" fill-rule="evenodd" clip-rule="evenodd"
-                         d="M8.30814 0C6.02576 0 4.33695 1.99763 4.41254 4.16407C4.48508 6.24542 4.39085 8.94102 3.7122 11.1393C3.03153 13.3441 1.88034 14.7407 0 14.92V16.9444C1.88034 17.1237 3.03153 18.5203 3.7122 20.7251C4.39085 22.9234 4.48508 25.619 4.41254 27.7003C4.33695 29.8664 6.02576 31.8644 8.30847 31.8644H31.6949C33.9773 31.8644 35.6658 29.8668 35.5902 27.7003C35.5176 25.619 35.6119 22.9234 36.2905 20.7251C36.9715 18.5203 38.1197 17.1237 40 16.9444V14.92C38.1197 14.7407 36.9715 13.3441 36.2905 11.1393C35.6119 8.94136 35.5176 6.24542 35.5902 4.16407C35.6658 1.99797 33.9773 0 31.6949 0H8.30814Z" />
-                     <path fill="white"
-                         d="M30.0474 8.81267L20.0721 26.7214C19.8661 27.0911 19.337 27.0933 19.128 26.7253L8.95492 8.81436C8.72711 8.41342 9.06866 7.92768 9.52124 8.009L19.5072 9.80102C19.5709 9.81245 19.6361 9.81234 19.6998 9.80069L29.4769 8.01156C29.928 7.92899 30.2711 8.41086 30.0474 8.81267Z" />
-                     <path fill="var(--primary-color)"
-                         d="M22.9634 11.0029L18.4147 11.8178C18.3784 11.8243 18.3455 11.8417 18.3211 11.8672C18.2967 11.8927 18.2823 11.9248 18.2801 11.9586L18.0003 16.2791C17.9937 16.3808 18.0959 16.4598 18.2046 16.4369L19.471 16.1697C19.5895 16.1447 19.6966 16.2401 19.6722 16.3491L19.2959 18.0335C19.2706 18.1468 19.387 18.2438 19.5081 18.2101L20.2903 17.9929C20.4116 17.9592 20.5281 18.0564 20.5025 18.1699L19.9045 20.8157C19.8671 20.9812 20.1079 21.0715 20.2083 20.9296L20.2754 20.8348L23.9819 14.0722C24.044 13.959 23.937 13.8299 23.8009 13.8539L22.4974 14.0839C22.3749 14.1055 22.2706 14.0012 22.3052 13.8916L23.156 11.1951C23.1906 11.0854 23.086 10.981 22.9634 11.0029Z" />
-                 </svg>
-                 <span class="fw-bold ps-2 fs-5 d-none d-xl-inline-flex text-gradient">BVITE</span>
-             </a>
-         </div>
-         <ul class="nav nav-tabs border-0 d-none d-xl-inline-flex" role="tablist">
-             <li class="nav-item" role="presentation">
-                 <div class="btn-group border rounded-pill" role="group"
-                     aria-label="Button group with nested dropdown">
-                     <div class="btn-group" role="group">
-                         <button type="button" class="btn bg-transparent rounded-pill dropdown-toggle"
-                             data-bs-toggle="dropdown" aria-expanded="false">Select App</button>
-                         <ul class="dropdown-menu p-2 p-xl-3 shadow rounded-4">
-                             <li><a class="dropdown-item rounded-pill active" href="#">BVite Sass App</a>
-                             </li>
-                             <li><a class="dropdown-item rounded-pill" href="#">BGulp Web App</a></li>
-                         </ul>
-                     </div>
-                     <button type="button" class="btn bg-transparent rounded-pill">
-                         <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="20" viewBox="0 0 24 24"
-                             stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                             <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-                             <path d="M9 12l6 0"></path>
-                             <path d="M12 9l0 6"></path>
-                         </svg>
-                     </button>
-                 </div>
-             </li>
-             <li class="nav-item" role="presentation">
-                 <a href="my-profile.html" class="nav-link">
-                     <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="22" viewBox="0 0 24 24"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                         <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-                         <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-                         <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
-                     </svg>
-                 </a>
-             </li>
-             <li class="nav-item" role="presentation">
-                 <a href="blog.html" class="nav-link">
-                     <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="22" viewBox="0 0 24 24"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                         <path
-                             d="M8 21h8a5 5 0 0 0 5 -5v-3a3 3 0 0 0 -3 -3h-1v-2a5 5 0 0 0 -5 -5h-4a5 5 0 0 0 -5 5v8a5 5 0 0 0 5 5z">
-                         </path>
-                         <path
-                             d="M7 7m0 1.5a1.5 1.5 0 0 1 1.5 -1.5h3a1.5 1.5 0 0 1 1.5 1.5v0a1.5 1.5 0 0 1 -1.5 1.5h-3a1.5 1.5 0 0 1 -1.5 -1.5z">
-                         </path>
-                         <path
-                             d="M7 14m0 1.5a1.5 1.5 0 0 1 1.5 -1.5h7a1.5 1.5 0 0 1 1.5 1.5v0a1.5 1.5 0 0 1 -1.5 1.5h-7a1.5 1.5 0 0 1 -1.5 -1.5z">
-                         </path>
-                     </svg>
-                 </a>
-             </li>
-             <li class="nav-item" role="presentation">
-                 <a href="#" class="nav-link">
-                     <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="22" viewBox="0 0 24 24"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                         <path
-                             d="M4 7h3a1 1 0 0 0 1 -1v-1a2 2 0 0 1 4 0v1a1 1 0 0 0 1 1h3a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h1a2 2 0 0 1 0 4h-1a1 1 0 0 0 -1 1v3a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-1a2 2 0 0 0 -4 0v1a1 1 0 0 1 -1 1h-3a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h1a2 2 0 0 0 0 -4h-1a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1">
-                         </path>
-                     </svg>
-                 </a>
-             </li>
-             <li class="nav-item" role="presentation">
-                 <a href="docs/changelog.html" class="nav-link">
-                     <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="22" viewBox="0 0 24 24"
-                         stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                         <path
-                             d="M10.09 4.01l.496 -.495a2 2 0 0 1 2.828 0l7.071 7.07a2 2 0 0 1 0 2.83l-7.07 7.07a2 2 0 0 1 -2.83 0l-7.07 -7.07a2 2 0 0 1 0 -2.83l3.535 -3.535h-3.988">
-                         </path>
-                         <path d="M7.05 11.038v-3.988"></path>
-                     </svg>
-                 </a>
-             </li>
-         </ul>
-     </div>
-     <div class="d-none layout-a-action">
-         <div class="mb-2">
-             <a class="d-flex align-items-center lh-sm p-0" href="#" role="button" data-bs-toggle="dropdown"
-                 aria-expanded="false" title="User">
-                 <img class="avatar rounded-circle border border-3 shadow" src="assets/images/profile_av.png"
-                     alt="avatar">
-             </a>
-             <div class="dropdown-menu dropdown-menu-end shadow p-2 p-xl-3 rounded-4">
-                 <div class="bg-body p-3 rounded-3">
-                     <h4 class="mb-1 title-font text-gradient">Michelle Glover</h4>
-                     <p class="small text-muted">michelle.glover@gmail.com</p>
-                     <input type="text" class="form-control form-control-sm" placeholder="Update my status">
-                 </div>
-                 <ul class="list-unstyled mt-3">
-                     <li>
-                         <a class="dropdown-item rounded-pill" aria-label="my wallet" href="my-wallet.html">
-                             <span class="align-middle">Balance: <span class="fw-bold text-success"><span
-                                         data-purecounter-start="0" data-purecounter-separator=","
-                                         data-purecounter-currency="$" data-purecounter-end="14000"
-                                         class="purecounter">0</span></span></span>
-                         </a>
-                     </li>
-                     <li><a class="dropdown-item rounded-pill" aria-label="my profile" href="my-profile.html">My
-                             Profile</a></li>
-                     <li><a class="dropdown-item rounded-pill" aria-label="my task" href="my-task.html">My
-                             Taskboard</a></li>
-                     <li><a class="dropdown-item rounded-pill" aria-label="account settings"
-                             href="account-settings.html">Settings</a></li>
-                     <li class="dropdown-divider"></li>
-                     <li><a class="dropdown-item rounded-pill" aria-label="Add another account" href="#">Add
-                             another account</a></li>
-                 </ul>
-                 <a class="btn py-2 btn-primary w-100 mt-3 rounded-pill" href="signin.html" role="button">Logout</a>
-                 <div class="mt-3 text-center small">
-                     <a class="text-muted me-1" href="#!">Privacy policy</a>•<a class="text-muted mx-1"
-                         href="#!">Terms</a>•<a class="text-muted ms-1" href="#!">Cookies</a>
-                 </div>
-             </div>
-         </div>
-         <a href="#" class="btn p-1" title="Sign out">
-             <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="28" height="28"
-                 viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round"
-                 stroke-linejoin="round">
-                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                 <path d="M7 6a7.75 7.75 0 1 0 10 0"></path>
-                 <path d="M12 4l0 8"></path>
-             </svg>
-         </a>
-     </div>
- </div>
+<div class="px-md-4 px-2 pt-2 pb-2 brand" data-bs-theme="none">
+    <div>
+        <div class="d-flex align-items-center pt-1">
+            <button class="btn d-inline-flex d-xl-none border-0 p-0 pe-2" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvas_Navbar">
+                <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24"
+                    stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M4 6l16 0"></path>
+                    <path d="M4 12l16 0"></path>
+                    <path d="M4 18l16 0"></path>
+                </svg>
+            </button>
+            <!--[ Start:: Brand Logo icon ]-->
+            <a href="index.html" class="brand-icon text-decoration-none d-flex align-items-center"
+                title="BVITE Admin Template">
+                <svg height="26" viewBox="0 0 40 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill="var(--primary-color)" fill-rule="evenodd" clip-rule="evenodd"
+                        d="M8.30814 0C6.02576 0 4.33695 1.99763 4.41254 4.16407C4.48508 6.24542 4.39085 8.94102 3.7122 11.1393C3.03153 13.3441 1.88034 14.7407 0 14.92V16.9444C1.88034 17.1237 3.03153 18.5203 3.7122 20.7251C4.39085 22.9234 4.48508 25.619 4.41254 27.7003C4.33695 29.8664 6.02576 31.8644 8.30847 31.8644H31.6949C33.9773 31.8644 35.6658 29.8668 35.5902 27.7003C35.5176 25.619 35.6119 22.9234 36.2905 20.7251C36.9715 18.5203 38.1197 17.1237 40 16.9444V14.92C38.1197 14.7407 36.9715 13.3441 36.2905 11.1393C35.6119 8.94136 35.5176 6.24542 35.5902 4.16407C35.6658 1.99797 33.9773 0 31.6949 0H8.30814Z" />
+                    <path fill="white"
+                        d="M30.0474 8.81267L20.0721 26.7214C19.8661 27.0911 19.337 27.0933 19.128 26.7253L8.95492 8.81436C8.72711 8.41342 9.06866 7.92768 9.52124 8.009L19.5072 9.80102C19.5709 9.81245 19.6361 9.81234 19.6998 9.80069L29.4769 8.01156C29.928 7.92899 30.2711 8.41086 30.0474 8.81267Z" />
+                    <path fill="var(--primary-color)"
+                        d="M22.9634 11.0029L18.4147 11.8178C18.3784 11.8243 18.3455 11.8417 18.3211 11.8672C18.2967 11.8927 18.2823 11.9248 18.2801 11.9586L18.0003 16.2791C17.9937 16.3808 18.0959 16.4598 18.2046 16.4369L19.471 16.1697C19.5895 16.1447 19.6966 16.2401 19.6722 16.3491L19.2959 18.0335C19.2706 18.1468 19.387 18.2438 19.5081 18.2101L20.2903 17.9929C20.4116 17.9592 20.5281 18.0564 20.5025 18.1699L19.9045 20.8157C19.8671 20.9812 20.1079 21.0715 20.2083 20.9296L20.2754 20.8348L23.9819 14.0722C24.044 13.959 23.937 13.8299 23.8009 13.8539L22.4974 14.0839C22.3749 14.1055 22.2706 14.0012 22.3052 13.8916L23.156 11.1951C23.1906 11.0854 23.086 10.981 22.9634 11.0029Z" />
+                </svg>
+                <span class="fw-bold ps-2 fs-5 d-none d-xl-inline-flex text-gradient">BVITE</span>
+            </a>
+        </div>
+        
+        <x-app.leftbar />
+
+    </div>
+    <div class="d-none layout-a-action">
+        <div class="mb-2">
+            <a class="d-flex align-items-center lh-sm p-0" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false" title="User">
+                <img class="avatar rounded-circle border border-3 shadow" src="assets/images/profile_av.png"
+                    alt="avatar">
+            </a>
+           <x-app.profile.action />
+        </div>
+        <a href="#" class="btn p-1" title="Sign out">
+            <svg class="svg-stroke" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
+                stroke="currentColor" fill="none" stroke-linecap="round"  stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M7 6a7.75 7.75 0 1 0 10 0"></path>
+                <path d="M12 4l0 8"></path>
+            </svg>
+        </a>
+    </div>
+</div>
